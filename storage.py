4e191b19f2cfa457f49640b783d35ed6facac328
Riley Thompson <rjthompson2@dons.usfca.edu>
2026-02-17 14:54:41 -0800

    encryption and decryption of data in individual user's files based on password

--- storage.py
+++ storage.py
@@ -1,23 +1,72 @@
 import hashlib
-import random
 import os
 import json
+import base64
+import hashlib
+from Crypto import Random
+from Crypto.Cipher import AES
 
-def add_login_info(user, username, password, website):
-    filename = "storage/"+user+".txt"
+class AESCipher:
+    def __init__(self, key):
+        self.bs = AES.block_size
+        self.key = self.key = hashlib.sha256(key.encode()).digest()
+
+    def encrypt(self, data:str):
+        data = self._pad(data)
+        iv = Random.new().read(AES.block_size)
+        cipher = AES.new(self.key, AES.MODE_CBC, iv)
+        return base64.b64encode(iv + cipher.encrypt(data.encode())).decode('latin-1')
+    
+    def decrypt(self, ciphertext):
+        ciphertext = base64.b64decode(ciphertext)
+        iv = ciphertext[:AES.block_size]
+        cipher = AES.new(self.key, AES.MODE_CBC, iv)
+        return AESCipher._unpad(cipher.decrypt(ciphertext[AES.block_size:])).decode('latin-1')
+
+    def _pad(self, s):
+        return s + (self.bs - len(s) % self.bs) * chr(self.bs - len(s) % self.bs)
+    
+    @staticmethod
+    def _unpad(s):
+        return s[:-ord(s[len(s)-1:])]
+
+
+def add_login_info(root_user, root_pw, username, password, website):
+    if not login(root_user, root_pw):
+        return
+    filename = "storage/"+root_user+".txt"
     if not os.path.exists(filename):
         return
+
+    data = get_login_info(root_user, root_pw)
+    if website in data.keys():
+        return
+    data[website] = {'username': username, 'password': password}
+
+    aes = AESCipher(root_pw)
+    data = aes.encrypt(json.dumps(data))
+    with open(filename, "w", encoding='latin-1') as f:
+        f.write(data)
+    
+
+
+def get_login_info(root_user, root_pw):
+    if not login(root_user, root_pw):
+        return
+    filename = "storage/"+root_user+".txt"
+    if not os.path.exists(filename):
+        return
+    
     with open(filename, "r", encoding='latin-1') as f:
         text = f.read()
         if text == "":
             return
-        data = json.loads(text)
-    if website in data.keys():
-        return
-    data[website] = {'username': username, 'password': password}
-    with open(filename, "w", encoding='latin-1') as f:
-        f.write(json.dumps(data))
-    
+    text = text.encode('latin-1')
+
+    aes = AESCipher(root_pw)
+    text = aes.decrypt(text)
+    return json.loads(text)
+
 def new_root_user(username, password):
     with open("storage/root_users.txt", "r", encoding='latin-1') as f:
         text = f.read()
@@ -28,18 +77,22 @@ def new_root_user(username, password):
     if username in users.keys():
         return
     else:
-        hashed_pw = salt_hash_auth_pw(password, random.randint(0, 10000).to_bytes(5, byteorder='little'))
+        salt = os.urandom(16) 
+        hashed_pw = salt_hash_auth_pw(password, salt)
         users[username] = hashed_pw.decode('latin-1')
         with open("storage/root_users.txt", "w", encoding='latin-1') as f:
             f.write(json.dumps(users))
-        new_user_file(username)
+        aes = AESCipher(password)
+        ciphertext = aes.encrypt("{}")
+        print(ciphertext)
+        new_user_file(username, ciphertext)
 
-def new_user_file(username):
+def new_user_file(username, input="{}"):
     path = "storage/"+username+".txt"
     if os.path.exists(path):
         os.remove(path)
     with open(path, "x") as f:
-        f.write("{}")
+        f.write(input)
 
 def login(username, password):
     with open("storage/root_users.txt", "r", encoding='latin-1') as f:
@@ -55,22 +108,26 @@ def login(username, password):
         return check_password(password, value)
 
 
-def salt_hash_auth_pw(pw, salt, cycles=2):
-    hashed_password = hashlib.pbkdf2_hmac('sha256', pw.encode('latin-1'), salt, 100000)
-    if cycles == 0:
-        return salt+"$".encode('latin-1')+hashed_password
-    return salt_hash_auth_pw(str(hashed_password), salt, cycles=cycles-1)
+def salt_hash_auth_pw(pw, salt, length=30):
+    hashed_password = hashlib.pbkdf2_hmac('sha256', pw.encode('latin-1'), salt, length)
+    return salt+"$".encode('latin-1')+hashed_password
 
 def check_password(pw, value):
-    verify_salt = ""
-    i = 0
-    while value[i] != ord("$"):
-        verify_salt += chr(value[i])
-        i+=1
+    verify_salt = get_salt(value)
     claimant_hash = salt_hash_auth_pw(pw, verify_salt.encode('latin-1'))
     return value == claimant_hash
+
+def get_salt(value:bytes):
+    salt = ""
+    i = 0
+    while value[i] != ord("$"):
+        salt += chr(value[i])
+        i+=1
+    return salt
 
 if __name__ == '__main__':
     new_root_user("test", "password")
     print(login("test", "password"))
-    add_login_info("test", "test123@gmail.com", "password", "https://test.url.com/login")
+    add_login_info("test", "password", "test123@gmail.com", "password", "https://test.url.com/login")
+    add_login_info("test", "password", "test123@gmail.com", "password", "https://test.url.com/login")
+    print(get_login_info("test", "password"))
